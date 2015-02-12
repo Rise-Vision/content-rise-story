58caceba82dddb1b6cec08a8e7bea9ed33c3f6bc
Mathew Meiers <mlm@risedisplay.com>
2015-02-12 14:03:00 -0600

    video-copy change

--- js/app.js
+++ js/app.js
@@ -152,8 +152,8 @@
         //Video Wallpaper Settings - alter the URL's to your converted videos		
         $("#video_element").wallpaper({
             source: {
-                mp4: "",
-                webm: "videos/alabama-v4.webm"
+                webm: "videos/market-wall-v5.webm",
+				mp4: "videos/market-wall-v5.mp4"
             }
         });
 
