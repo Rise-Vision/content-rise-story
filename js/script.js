8eeb1603a858d43b8279a660364500b89d178edf
pcsandford <petersandford1982@gmail.com>
2015-03-03 11:00:49 -0500

    changes to QR code animation and addition of XML function

--- js/script.js
+++ js/script.js
@@ -93,6 +93,38 @@ $(document).ready(function() {
                 new RTP.PinchZoom($(this), {});
             });
 
+
+		//QR code button animation
+
+	var closeButton = function(){
+		$('.QRcode').removeClass('slideAnimation').addClass('slideAnimationDown');
+	}	
+
+	var closeButtonTimer;
+	
+    $('button').on('click',function(){
+    if ($('.QRcode').hasClass('slideAnimation')){
+    	$('.QRcode').removeClass('slideAnimation').addClass('slideAnimationDown');
+    	clearTimeout(closeButtonTimer);
+    } else {
+    	$('.QRcode').removeClass('slideAnimationDown').addClass('slideAnimation');
+    	closeButtonTimer = setTimeout(closeButton,12000);
+    }
+    });
+
+// 	$(document).ready(function () {
+    
+//     $('button').click( function () {
+//         $('#message-box').delay(300).slideDown('slow');
+//         $('.QRcode').delay(700).css('display','block');
+//     });
+    
+// });
+
+
+
+    
+
 	 });
 
 	
