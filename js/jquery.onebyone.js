dabbb346ca0080be0b0a85a5a77a2eb993f6881a
Mathew Meiers <mlm@risedisplay.com>
2015-03-11 09:50:14 -0500

    changed tolerance

--- js/jquery.onebyone.js
+++ js/jquery.onebyone.js
@@ -118,7 +118,7 @@ $(document).ready(function() {
 			isMouseDown = false;
 			if (!xdiff) return false;
 			var fullWidth = parseInt(settings.width);
-			var halfWidth = fullWidth / 2;
+			var halfWidth = fullWidth / 4;
 			if (-xdiff > halfWidth - fullWidth * settings.tolerance) {
 				_n++;
 				_n = _n >= bannerNum ? bannerNum - 1 : _n;
@@ -167,7 +167,7 @@ $(document).ready(function() {
 				var _n = currentBannerNum;
                 if (!xdiff) return false;
                 var fullWidth = parseInt(settings.width);
-                var halfWidth = fullWidth / 6;
+                var halfWidth = fullWidth / 4;
                 if (-xdiff > halfWidth - fullWidth * settings.tolerance) {
                     _n++;
                     _n = _n >= bannerNum ? bannerNum - 1 : _n;
